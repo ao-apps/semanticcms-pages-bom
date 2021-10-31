f3b132e6ac629905191a4249e76ced3e20bb3487
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:49:04 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- book/src/main/java/module-info.java
+++ book/src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with semanticcms-pages-bom.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-pages-bom.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.pages.bom.book {
 	// Direct
